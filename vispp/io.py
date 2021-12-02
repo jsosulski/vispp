2efb4b52c0605710750ea4b26bde4df65e235c9d
Jan Sosulski <mail@jan-sosulski.de>
2021-12-02 14:19:52 +0100

    Added helper for better pdf output behaviour in git repos

---
+++ vispp/io.py
@@ -0,0 +1,14 @@
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+def better_savefig(fig, figfile, **kwargs):
+    """To be used instead of .savefig
+
+    This function saves pdfs without creation date. So subsequent
+    overwrites of pdf files does not cause e.g. git modified.
+    """
+    if format == "pdf":
+        with PdfPages(figfile, metadata={"CreationDate": None}) as pdf:
+            pdf.savefig(fig)
+    else:
+        fig.savefig(figfile, *kwargs)
